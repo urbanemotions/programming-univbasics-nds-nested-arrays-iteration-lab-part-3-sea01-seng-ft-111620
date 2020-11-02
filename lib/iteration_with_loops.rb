4571d58587d13e550041f0dc94772854fa317b83
Sin Wildy <sw.wildy@gmail.com>
2020-11-02 04:35:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
       if !src[row_index][element_index].is_a? Integer
         output_string << src[row_index][element_index]
-        output_string << " "
+        #output_string << " "
         output_string << single_string_value
       end
       element_index += 1
